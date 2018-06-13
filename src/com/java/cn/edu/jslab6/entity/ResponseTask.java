943ba804afbc2998a5d7c56f2c494c57f3a70e31
ffzhello <314094371@qq.com>
2018-06-13 21:48:13 +0800

    "hello world"

--- src/com/java/cn/edu/jslab6/entity/ResponseTask.java
+++ src/com/java/cn/edu/jslab6/entity/ResponseTask.java
@@ -3,6 +3,6 @@ package com.java.cn.edu.jslab6.entity;
 public class ResponseTask {
 
     public static void main(String[] args) {
-        System.out.println("xxx");
+        System.out.println("hello world!");
     }
 }
