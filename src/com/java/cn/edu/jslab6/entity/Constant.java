9fac93917abe2c68e367b47139cdbad9f58c412d
ffzhello <314094371@qq.com>
2018-06-16 07:44:58 +0800

    常量定义

---
+++ src/com/java/cn/edu/jslab6/entity/Constant.java
@@ -0,0 +1,12 @@
+package com.java.cn.edu.jslab6.entity;
+
+/**
+ * @author Andy
+ * @date 18-6-15 下午4:48
+ * @description 所有的常量定义
+ */
+public final class Constant {
+
+    //限制TMP文件的最大2M
+    public static final int MAX_TMP_FILE_SIZE = 2 * 1024 * 1024;
+}
